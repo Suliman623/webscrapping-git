ae6b82d9a4cb93bacfb76240d0b47490eb2a5813
Suliman Riaz <124143168+Suliman623@users.noreply.github.com>
2024-07-20 09:03:21 +0500

    Update webscrappin_selenium.py

--- webscrappin_selenium.py
+++ webscrappin_selenium.py
@@ -1,32 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import os
+
+# Initialize WebDriver
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Optional: run headless for no UI
+driver = webdriver.Chrome(service=Service('/path/to/chromedriver'), options=chrome_options)
 
 github_user = input('Input Github User: ')
 
-
-driver = webdriver.Chrome()
-
 # Load the GitHub user profile page
-url = 'https://github.com/' + github_user
+url = f'https://github.com/{github_user}'
 driver.get(url)
 
 # Fetch profile image
-avatar_element = driver.find_element_by_xpath('//*[@alt="Avatar"]')
+avatar_element = driver.find_element(By.XPATH, '//*[@alt="Avatar"]')
 profile_image = avatar_element.get_attribute('src')
 print("Profile Image:", profile_image)
 
 # Fetch number of repositories
-repositories_element = driver.find_element_by_xpath("//a[contains(@class, 'UnderlineNav-item') and contains(@href, '/{0}?tab=repositories')]/span".format(github_user))
+repositories_element = driver.find_element(By.XPATH, "//a[contains(@href, '?tab=repositories')]/span")
 repositories_count = repositories_element.text.strip()
 print("Number of Repositories:", repositories_count)
 
 # Fetch number of followers
-followers_element = driver.find_element_by_xpath("//a[contains(@href, '?tab=followers')]/span[@class='text-bold color-fg-default']".format(github_user))
+followers_element = driver.find_element(By.XPATH, "//a[contains(@href, '?tab=followers')]/span[@class='text-bold color-fg-default']")
 followers_count = followers_element.text.strip()
 print("Number of Followers:", followers_count)
 
 # Fetch number of following
-following_element = driver.find_element_by_xpath("//a[contains(@href, '?tab=following')]/span[@class='text-bold color-fg-default']".format(github_user))
+following_element = driver.find_element(By.XPATH, "//a[contains(@href, '?tab=following')]/span[@class='text-bold color-fg-default']")
 following_count = following_element.text.strip()
 print("Number of Following:", following_count)
 
